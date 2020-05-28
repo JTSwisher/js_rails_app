c9e50de57cccfdecc4a7c6cf144aa86d5433ca2c
JTSwisher <jefftswisher@gmail.com>
2020-05-27 20:43:47 -0700

    Add check for valid user if false create user, validate user login set session id

--- backend_api/app/controllers/users_controller.rb
+++ backend_api/app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
 
     def create
-        #raise binding.pry
-        user = User.create(username: params["username"], password:["password"])
+        user= User.find_by(username: params["username"])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+        else
+            user = User.create(username: params["username"], password:["password"])
+            session[:user_id] = user.id
+        end 
     end 
 
     private 
 
-   # def user_params
-    #    params.require(:user).permit(:username, :password)
-   # end
+    def user_params
+       params.require(:user).permit(:username, :password)
+    end
 
 end
