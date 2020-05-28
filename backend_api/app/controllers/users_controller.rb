10f633e814910d7d060506635cac5924ba9c068b
JTSwisher <jefftswisher@gmail.com>
2020-05-27 20:13:22 -0700

    Got create user action working after succesful fetch request

--- backend_api/app/controllers/users_controller.rb
+++ backend_api/app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
     def create
-
+        #raise binding.pry
+        user = User.create(username: params["username"], password:["password"])
     end 
 
     private 
 
-    def user_params
-        binding.pry
-    end
+   # def user_params
+    #    params.require(:user).permit(:username, :password)
+   # end
 
 end
--- backend_api/app/models/user.rb
+++ backend_api/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :gifs
 
     validates :username, presence: true, uniqueness: true
